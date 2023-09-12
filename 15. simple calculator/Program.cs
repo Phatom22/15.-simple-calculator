b48f2254ba2622b2092198395cd40bbb798a2d26
nomvula <nzshabangu95@gmail.com>
2023-09-12 14:30:36 +0200

    Add project files.

---
+++ 15. simple calculator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.simple_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter first number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter operand");
+            string ope = Console.ReadLine();
+
+            Console.WriteLine("enter second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (ope == "+")
+            {
+                Console.WriteLine(" the answer is = " + (num1 + num2));
+            }
+            else if(ope == "-")
+            {
+                Console.WriteLine(" the answer is = " + (num1 - num2));
+            }
+            else if (ope == "*")
+            {
+                Console.WriteLine(" the answer is = " +(num1 * num2));
+            }
+            else if (ope == "/")
+            {
+                Console.WriteLine(" the answer is = "+ (num1 / num2));
+            }
+            else
+            {
+                Console.WriteLine(" the operand not found");
+            }
+
+            //freeze line
+            Console.ReadLine();
+        }
+    }
+}
